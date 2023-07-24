990da0d1b077c882bcdb16902baeb23344974176
codemonkey415 <lastsnowleopard415@gmail.com>
2023-07-23 21:07:51 -0400

    optimized

--- models/clothModel.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type ClothData struct {
-	Heading     string   `bson:"heading" json:"heading" validate:"required"`
-	Description string   `bson:"description" json:"description" validate:"required"`
-	Gender      string   `bson:"gender" json:"gender" validate:"required"`
-	Img         []string `bson:"img" json:"img" validate:"required"`
-}
