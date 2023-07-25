eb375859dcb267cf874a5b972ced3437f4bd274e
codemonkey415 <lastsnowleopard415@gmail.com>
2023-07-25 10:54:22 -0400

    add sendMail.go

---
+++ helpers/sendMail.go
@@ -0,0 +1,102 @@
+// package helper
+
+// import (
+// 	"fmt"
+// 	"net/smtp"
+// )
+
+// func SendMail(email string, data string) {
+
+// 	fmt.Println("data", data)
+
+// 	// Sender data.
+// 	from := "foryoursuccess0415@gmail.com"
+// 	password := "ukubrwzsfcxwjoih"
+
+// 	// Receiver email address.
+// 	to := []string{
+// 		email,
+// 	}
+
+// 	// smtp server configuration.
+// 	smtpHost := "smtp.gmail.com"
+// 	smtpPort := "587"
+
+// 	// Message.
+// 	message := []byte(data)
+
+// 	fmt.Println("message", message)
+
+// 	// Authentication.
+// 	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+// 	// Sending email.
+// 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println("Email Sent Successfully!")
+// }
+
+package helper
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+	"strings"
+)
+
+type Mail struct {
+	Sender  string
+	To      []string
+	Subject string
+	Body    string
+}
+
+func SendMail(email string, data string) {
+
+	sender := "foryoursuccess0415@gmail.com"
+
+	to := []string{
+		email,
+	}
+
+	user := "foryoursuccess0415@gmail.com"
+	password := "ukubrwzsfcxwjoih"
+
+	subject := "Simple HTML mail"
+	body := `<pre>` + data + `</pre>`
+
+	request := Mail{
+		Sender:  sender,
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}
+
+	addr := "smtp.gmail.com:587"
+	// host := "smtp.mailtrap.io"
+	host := "smtp.gmail.com"
+
+	msg := BuildMessage(request)
+	auth := smtp.PlainAuth("", user, password, host)
+	err := smtp.SendMail(addr, auth, sender, to, []byte(msg))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Email sent successfully")
+}
+
+func BuildMessage(mail Mail) string {
+	msg := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
+	msg += fmt.Sprintf("From: %s\r\n", mail.Sender)
+	msg += fmt.Sprintf("To: %s\r\n", strings.Join(mail.To, ";"))
+	msg += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+	msg += fmt.Sprintf("\r\n%s\r\n", mail.Body)
+
+	return msg
+}
